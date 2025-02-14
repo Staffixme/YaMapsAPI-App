1b5079ca3882d023ca8c0efd949eac5b12067681
Sys4 <metelkin_2007@mail.ru>
2025-02-14 17:24:54 +0900

    2 tasks

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
 import sys
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.uic import loadUi
+from PyQt6.QtGui import QPixmap
+import requests
+
 
 class MapWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+        loadUi("main_window.ui", self)
+
+        self.search_button.clicked.connect(self.search_map)
+        self.z = None
+
+    def update_map(self, x, y, size):
+        try:
+            link = (f"https://static-maps.yandex.ru/v1?lang=ru_RU&ll={x},{y}"
+                    f"&z={size}&apikey={self.apikey}")
+            print(f"Запрос: {x}, {y}, {size}")
+            result = requests.get(link)
+            content = result.content
+            status = result.status_code
+            if status == 200:
+                pixmap = QPixmap()
+                pixmap.loadFromData(content)
+                self.map.setPixmap(pixmap)
+            else:
+                print(status, "Что-то пошло не так: ", result.reason)
+        except Exception as e:
+            print("Получено исключение:", e)
+
+    def search_map(self):
+        try:
+            self.x = float(self.line_x.text())
+            self.y = float(self.line_y.text())
+            self.z = int(self.line_z.text())
+            self.update_map(self.x, self.y, self.z)
+        except ValueError:
+            print("Некорректный ввод данных.")
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            if self.z is not None:
+                self.z = min(self.z + 1, 17)
+                print('Увеличиваем масштаб')
+                self.update_map(self.x, self.y, self.z)
+        elif event.key() == Qt.Key.Key_PageDown:
+            if self.z is not None:
+                self.z = max(self.z - 1, 1)
+                print('Уменьшаем масштаб')
+                self.update_map(self.x, self.y, self.z)
 
 
 if __name__ == "__main__":
